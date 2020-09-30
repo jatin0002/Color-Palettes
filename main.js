69765df5d817b3f5d07cb049f3fd2c7b439eadb3
ishaan-18 <ishaan-18>
2020-10-01 00:24:33 +0530

    Change change variables

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-const layers = document.querySelectorAll(".layer").forEach((item) => {
-  item.addEventListener("dblclick", function () {
+const layers = document.querySelectorAll(".layer").forEach((itm) => {
+  itm.addEventListener("dblclick", function () {
     let randomNo2 = Math.floor(Math.random() * 255);
     let randomNo1 = Math.floor(Math.random() * 255);
     let randomNo3 = Math.floor(Math.random() * 255);
     let colorNumber = `rgb(${randomNo1} , ${randomNo2} , ${randomNo3})`;
-    item.style.backgroundColor = colorNumber;
+    itm.style.backgroundColor = colorNumber;
 
     this.textContent = colorNumber;
   });
